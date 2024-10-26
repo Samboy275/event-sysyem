fc6047eeb7afae97f30875c3e40f260ee3c63048
samboy275 <sam-wal@windowslive.com>
2024-10-26 23:06:39 +0300

    Add members handling to organization views

--- app/views/organization_views/routes.py
+++ app/views/organization_views/routes.py
@@ -14,6 +14,8 @@ def create_organization():
         name = data.get("name")
         description = data.get("description")
 
+
+        members = list(data.get("members"))
         user = get_jwt_identity()
 
         db = get_db()
@@ -22,22 +24,23 @@ def create_organization():
         user_collection = db.users
 
         admin_user = user_collection.find_one({"email" : user})
-
+        admin_user.pop("_id")
+        admin_user.pop("password")
+        admin_user["access_level"] = "admin"
+        members.insert(0, admin_user)
         result = org_collection.insert_one({
             "name" : name,
             "description" : description,
-            "members" : [
-                {"name" : admin_user["name"],
-                "email": admin_user["email"],
-                "access_level":"admin"},
-            ]
+            "members" : members
         })
 
         return jsonify(organization_id = str(result.inserted_id)), 201
 
 
 
-
+def serialize_members(records):
+    for record in records:
+        record.pop("_id")
 
 @jwt_required()
 def get_organization(organization_id):
